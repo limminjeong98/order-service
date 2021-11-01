272168a56297d5fa696c8b7c7cf2e8c7e09203af
HKMC\E183999 <limminjeong98@naver.com>
2021-11-01 16:46:13 +0900

    Orders Microservice - 기능 구현 2 오류 수정

--- src/main/java/com/example/orderservice/jpa/OrderEntity.java
+++ src/main/java/com/example/orderservice/jpa/OrderEntity.java
@@ -17,15 +17,19 @@ public class OrderEntity implements Serializable {
 
     @Column(nullable = false, length = 120, unique = true)
     private String productId;
+
     @Column(nullable = false)
     private Integer qty;
+
     @Column(nullable = false)
     private Integer unitPrice;
+
     @Column(nullable = false)
     private Integer totalPrice;
 
     @Column(nullable = false)
     private String userId;
+
     @Column(nullable = false, unique = true)
     private String orderId;
 
--- src/main/java/com/example/orderservice/vo/RequestOrder.java
+++ src/main/java/com/example/orderservice/vo/RequestOrder.java
@@ -1,5 +1,8 @@
 package com.example.orderservice.vo;
 
+import lombok.Data;
+
+@Data
 public class RequestOrder {
     private String productId;
     private Integer qty;
